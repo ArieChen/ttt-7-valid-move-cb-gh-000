e80a990a50b6beaf2fb977bc113094ea929c34c0
Arie Tando <13510018@std.stei.itb.ac.id>
2018-01-25 06:28:21 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -5,6 +5,6 @@ end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, index)
-  puts board[index]
+  puts board[index].strip
   (board[index] != nil || board[index].strip == "") ? false : true
 end
